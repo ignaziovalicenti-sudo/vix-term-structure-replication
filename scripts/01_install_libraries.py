fd5bc826cfe8b5a22e8ebf59378f66a2dd50a7a6
ignaziovalicenti-sudo <ignazio.valicenti@gmail.com>
2026-09-06 16:53:03 -0300

    Update 01_install_libraries.py

--- scripts/01_install_libraries.py
+++ scripts/01_install_libraries.py
@@ -1,1 +1,28 @@
+# ==============================================================
+# scripts/01_install_libraries.py
+# ETAPA 2.3 - Preparar entorno de Google Colab
+#
+# Objetivo:
+# Instalar las librerías necesarias para ejecutar la Etapa 2.
+# ==============================================================
 
+import sys
+import subprocess
+
+packages = [
+    "pandas",
+    "numpy",
+    "yfinance",
+    "pyarrow"
+]
+
+subprocess.check_call([
+    sys.executable,
+    "-m",
+    "pip",
+    "install",
+    *packages,
+    "-q"
+])
+
+print("Librerías instaladas correctamente.")
